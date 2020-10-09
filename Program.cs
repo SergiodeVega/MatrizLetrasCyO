489cef7d7ab4b20d0e8d22deb184440bb1b4290c
Sergio de Vega <sergiodevega@hotmail.com>
2020-10-09 20:23:15 +0200

    primer commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace matrizletras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] letra = genera('O');
+            imprime(letra);
+            Console.WriteLine("\n");
+            letra = genera('C');
+            imprime(letra);
+        }
+
+        static public char[,] genera(char letra) {
+            char[,] matr = new char[7,7];
+            switch(letra) {
+                case 'O':
+                    matr = new char[7,7] {  {' ',' ','*','*','*',' ',' '},
+                                            {' ','*',' ',' ',' ','*',' '},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {' ','*',' ',' ',' ','*',' '},
+                                            {' ',' ','*','*','*',' ',' '}
+                    };
+                    break;
+                case 'C':
+                    matr = new char[7,7] {  {' ',' ','*','*','*','*','*'},
+                                            {' ','*',' ',' ',' ',' ',' '},
+                                            {'*',' ',' ',' ',' ',' ',' '},
+                                            {'*',' ',' ',' ',' ',' ',' '},
+                                            {'*',' ',' ',' ',' ',' ',' '},
+                                            {' ','*',' ',' ',' ',' ',' '},
+                                            {' ',' ','*','*','*','*','*'}
+                    };
+                    break;
+               // default:
+
+            }
+            return matr;
+        }
+
+        static public void imprime(char[,] matr) {
+            for (int fila=0; fila < 7; fila++) {
+                for (int colu=0; colu<7; colu++) {
+                    Console.Write(matr[fila,colu]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+ 
+    
+}
